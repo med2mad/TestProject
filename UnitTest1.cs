0ad291590a917e55a2c64a53615f9b54bb20c8a1
med2mad <mohamed.leghdaich@gmail.com>
2025-07-04 14:24:02 +0100

    MOQs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,11 +1,34 @@
-﻿namespace TestProject1
+﻿
+using Moq;
+
+namespace TestProject1;
+
+public class UnitTest1
 {
-    public class UnitTest1
+    [Fact]
+    public void Test1()
     {
-        [Fact]
-        public void Test1()
-        {
-            Assert.True(false);
-        }
+        Mock<IT> mock1 = new();
+
+        //mock1.Setup(x => x.F(It.IsAny<int>())).Returns(5);
+        //Assert.Equal(5, mock1.Object.F(1));
+
+        //mock1.Setup(x => x.F(It.IsAny<int>())).Throws<Exception>();
+        //Assert.Throws<Exception>(()=>mock1.Object.F(0));
+
+        mock1.Object.F(4);
+        mock1.Verify(x => x.F(It.IsAny<int>()), Times.AtLeast(2));
+
+
+    }
+}
+
+
+public class IT
+{
+    public virtual int F(int a)
+    {
+        Console.WriteLine("ewq");
+        return a;
     }
 }
